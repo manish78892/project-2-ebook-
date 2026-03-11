13efeda3361642f9143734b59f3ec76ccfde539d
manish78892 <manishpanwar78892@gmail.com>
2026-03-11 14:14:13 +0530

    mongo udate

--- init/index.js
+++ init/index.js
@@ -1,31 +1,33 @@
 const mongoose = require('mongoose');
+require('dotenv').config({ path: '../.env' });
+
 const bookData = require('./bookdata.js');
 const Book = require('../models/book.js');
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/ebook';
-
-main()
-.then(()=>{
-    console.log('Connected to MongoDB');
-})
-.catch((err)=>{
-    console.error(err);
-});
-async function main (){
-    await mongoose.connect(MONGO_URL);
+async function main() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to MongoDB Atlas");
+    } catch (err) {
+        console.error("Connection error:", err);
+    }
 }
 
 const initDB = async () => {
     try {
         await Book.deleteMany({});
-        console.log('Existing books deleted');
+        console.log("Existing books deleted");
+
         await Book.insertMany(bookData.bookdata);
-        console.log('Books inserted');
+        console.log("Books inserted");
+
     } catch (err) {
         console.error(err);
     } finally {
         mongoose.connection.close();
     }
-}
+};
 
-initDB();
+main().then(() => {
+    initDB();
+});
